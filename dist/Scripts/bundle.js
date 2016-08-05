5a52d73538131795fc09b9a941eb693e822c7cb4
Mark Eads <meads@hcpci.com>
2016-08-05 16:15:02 -0400

    react router module

--- dist/Scripts/bundle.js
+++ dist/Scripts/bundle.js
@@ -49570,6 +49570,22 @@ $ = jQuery = require('jquery');
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+
+    statics: {
+        willTransitionTo : function(transition, params, query, callback) {
+            if(!confirm('Are you sure you want to read this page?')) { 
+                 
+            }
+            else {
+                callback();
+            }
+        },
+         willTransitionFrom : function(transition, component) {
+            if(!confirm('Are you sure you want to leave this page?')) { 
+          
+            }
+        }
+    },
     render : function() { 
         return (
             React.createElement("div", {className: "container jumbotron"}, 
--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -2,6 +2,22 @@ $ = jQuery = require('jquery');
 var React = require('react');
 
 var About = React.createClass({
+
+    statics: {
+        willTransitionTo : function(transition, params, query, callback) {
+            if(!confirm('Are you sure you want to read this page?')) { 
+                 
+            }
+            else {
+                callback();
+            }
+        },
+         willTransitionFrom : function(transition, component) {
+            if(!confirm('Are you sure you want to leave this page?')) { 
+          
+            }
+        }
+    },
     render : function() { 
         return (
             <div className="container jumbotron">
