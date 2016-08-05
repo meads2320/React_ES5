73ba29f11f9381d8b429555f95907462af95396c
Mark Eads <meads@hcpci.com>
2016-08-05 15:28:18 -0400

    added the react router

--- src/main.js
+++ src/main.js
@@ -1,36 +1,8 @@
-$ = jQuery = require('jquery');
+
 var React = require('react');
-var Home = require('./components/home/homePage');
-var About =  require('./components/about/aboutPage');
-var Authors =  require('./components/authors/authorsPage');
+var Router = require('react-router');
+var routes = require('./routes');
 
-var Header =  require('./components/common/header');
-var App = React.createClass({
-    render: function() { 
-        var Child;
-        
-        switch(this.props.route) {
-            case 'about': 
-                Child = About;
-                break;
-            case 'authors':
-                Child = Authors;
-                break;
-            default : Child = Home;
-        }
-        return (
-            <div>
-                <Header />
-                <Child />
-            </div>
-        );
-    }
-})
-
-function render() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />,  document.getElementById('app'))
-}
-
-window.addEventListener('hashChange', render);
-render();
+Router.run(routes, function(Handler) { 
+    React.render(<Handler/>, document.getElementById('app'));
+});
